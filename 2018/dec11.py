b0c3b3f8ae4b27586ecbd4b20cb3430d77b52d8e
jesper hogstrom <jspr.hgstrm@gmail.com>
2018-12-11 18:01:27 +0100

    Solution for day 11, star 2 - can also solve *1 with some tweaking

--- 2018/dec11.py
+++ 2018/dec11.py
@@ -1,7 +1,10 @@
 from collections import defaultdict
 serial = 3031
-grid = defaultdict(list)
-gridsum = defaultdict(list)
+#serial = 42
+#serial = 18
+
+SIZE=300
+SEARCHAREA=50
 
 def powerlevel(x, y, s):
 	v = x
@@ -9,7 +12,7 @@ def powerlevel(x, y, s):
 	v *= y
 	v += s
 	v *= (x+10)
-	return int(str(v).zfill(5)[-3:-2]) - 5
+	return int(str(v).zfill(3)[-3:-2]) - 5
 
 #print(powerlevel(3, 5, 8))
 
@@ -19,43 +22,71 @@ def powerlevel(x, y, s):
 #exit()
 
 
-for y in range(300):
-	for x in range(300):
-#		v = x+1
-#		v = x + 10
-#		v *= (y+1)
-#		v += serial
-#		v *= (x+10)
-#		v = str(v).zfill(5)[-3:-2]
-		#v = int(str((((x+10)*y)+serial)*(x+10)).zfill(5)[-3:-2])
-		grid[y].append(powerlevel(x+1, y+1, serial))
+def creategrid():
+	grid = defaultdict(list)
+	for y in range(SIZE):
+		for x in range(SIZE):
+			grid[y].append(powerlevel(x+1, y+1, serial))
+	return grid
 
+def addgridsums(grid):
+	row = tree = lambda: defaultdict(list)
+	gridsum = defaultdict(row)
+	for y in range(SIZE):
+		for x in range(SIZE):
+			gridsum[y][x] = []
+#			print(grid[y][x:10])
+			for i in range(x+1, x+1+SEARCHAREA):
+#				if i < 10:
+#					print(x, i, sum(grid[y][x:i]), grid[y][x:i])
+				gridsum[y][x].append(sum(grid[y][x:i]))
+#			gridsum[y].append(sum(grid[y][x:x+3]))
+		if y % 50 == 0:
+				print("added", y)
+	return gridsum
 
-maxval = 0
-savex, savey = 0, 0
-for y in range(300):
-	for x in range(300-3):
-		gridsum[y].append(sum(grid[y][x:x+3]))
+def getareavalue(gridsum, x, y, size):
+	if x + size >= SIZE:
+		return 0
+	s = 0
+	for dy in range(y, min(SIZE, y+size)):
+		#print(dy, x)
+		s += gridsum[dy][x][size-1]
+	#s = gridsum[y][x][size-1] + gridsum[y+1][x][size-1] + gridsum[y+2][x][size-1]
+	return s
 
-#print(len(gridsum[1]))
-#print(gridsum[1])
-#exit()
-for y in range(300-3):
-	if y % 50 == 0:
-		print(y)
-	for x in range(300-3):
-#		print(x, y, len(gridsum[y]))
-		s = gridsum[y][x] + gridsum[y+1][x] + gridsum[y+2][x]
-		if s > maxval:
-			maxval = s
-			savex = x
-			savey = y
+def findmax(grid, gridsum):
+	maxval = 0
+	savex, savey, maxsize = 0, 0, 0
+	for y in range(SIZE):
+		if y % 50 == 0:
+			print("scanned", y)
+		for x in range(SIZE):
+			for i in range(1, SEARCHAREA):
+				s = getareavalue(gridsum, x, y, i)
+				if s > maxval:
+					maxval = s
+					savex = x
+					savey = y
+					maxsize = i
+	return [savex+1, savey+1, maxval, maxsize]
 
+def printsector(grid, coord, size):
+	for y in range(coord[1]-1, coord[1]+size+1):
+		r = ""
+		for x in range(coord[0]-1, coord[0]+size+1):
+			fstr = " {0:2} "
+			if x in range(coord[0], coord[0]+size) and y in range(coord[1], coord[1]+size):
+				fstr = "[{0:2}]"
 
-print("{0},{1}".format(savex+1, savey+1))
+			r += fstr.format(grid[y-1][x-1])
+		print(r)
 
-for y in range(savey - 3, savey + 6):
-	r = ""
-	for x in range(savex - 3, savex + 6):
-		r += " {0:3} ".format(grid[y][x])
-	print(r)
+grid = creategrid()
+gridsum = addgridsums(grid)
+res = findmax(grid, gridsum)
+printsector(grid, res, res[-1])
+
+print("Result:", res)
+
+print("{0},{1},{2}".format(res[0], res[1], res[3]))
