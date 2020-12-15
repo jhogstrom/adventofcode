e03c3e50be7b49a59f773d125859b84a5d4e3b18
HOGSTROM Jesper <jesper.hogstrom@leica-geosystems.com>
2020-12-15 12:31:32 +0100

    Improved output from timing decorator

--- 2020/timing.py
+++ 2020/timing.py
@@ -9,6 +9,13 @@ def timeit(method):
             name = kw.get('log_name', method.__name__.upper())
             kw['log_time'][name] = int((te - ts) * 1000)
         else:
-            print(f'{method.__name__}: {(te - ts)*1000:2.3f} ms')
+            name = f'{method.__name__}{args}'
+            duration_ms = (te - ts)*1000
+            if duration_ms < 1:
+                print(f'{name}: {int(duration_ms*1000)}us')
+            elif duration_ms < 1000:
+                print(f'{name}: {duration_ms:2.3f}ms')
+            else:
+                print(f'{name}: {duration_ms/1000:2.2f}s')
         return result
     return timed
