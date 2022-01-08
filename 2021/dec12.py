4fc9b429f7e62f6c1258aaf8d0b40b9127e084dd
Jesper Högström <jesper.hogstrom@aditrologistics.com>
2022-01-09 00:43:35 +0100

    Reduced string comparisons, 40% speed up

--- 2021/dec12.py
+++ 2021/dec12.py
@@ -5,7 +5,7 @@ from collections import defaultdict, deque
 
 stardate = 12
 dataname = f"dec{stardate}.txt"
-dataname = f"dec{stardate}_test1.txt" # star1/star2: 10/36
+# dataname = f"dec{stardate}_test1.txt" # star1/star2: 10/36
 # dataname = f"dec{stardate}_test.txt" # star1/star2: 19/103
 # dataname = f"dec{stardate}_test2.txt" # star1/star2: 226/3509
 curdir = os.path.dirname(os.path.abspath(__file__))
@@ -13,36 +13,35 @@ filename = f'{curdir}\\{dataname}'
 data = [_.strip().split("-") for _ in open(filename, 'r').readlines()]
 
 class Node():
-    def __init__(self, start) -> None:
-        self.start = start
+    def __init__(self, node_name) -> None:
+        self.node_name = node_name
         self.next: List[Node] = []
+        self.is_small = self.node_name != self.node_name.upper()
+        self.is_start = node_name == "start"
 
     def add_next(self, nodes):
         self.next.extend(nodes)
 
-    @property
-    def is_small(self):
-        return (self.start != self.start.upper())
 
 def check_star2(trail: List[Node], node: Node) -> bool:
     counter = defaultdict(int)
-    for t in trail + [node]:
-        if t.is_small:
-            counter[t.start] += 1
+    for t in [_ for _ in trail + [node] if _.is_small]:
+        counter[t.node_name] += 1
 
-    visits = list(counter.values()) + [0]
-    res = max(visits) <= 2 and visits.count(2) <= 1
-    return res
+    visits = list(counter.values())
+    return max(visits) <= 2 and visits.count(2) <= 1
+
 
 def check_star1(trail: List[Node], node: Node) -> bool:
     return node not in trail
 
-def traverse(start: Node, target, trail: List[Node], check):
+
+def traverse(node: Node, target, trail: List[Node], check):
     res = 0
-    if start.start == target:
-        print(" - ".join(_.start for _ in trail))
+    if node.node_name == target:
+        # print(" - ".join(_.start for _ in trail))
         return 1
-    for n in (_ for _ in start.next if _.start != "start"):
+    for n in (_ for _ in node.next if not _.is_start):
         if not n.is_small or check(trail, n):
             res += traverse(n, target, trail + [n], check)
 
@@ -52,36 +51,33 @@ def traverse(start: Node, target, trail: List[Node], check):
 def make_nodes(data) -> List[Node]:
     nodes = []
     for d in data:
-        if not any(_ for _ in nodes if _.start == d[0]):
+        if not any(_ for _ in nodes if _.node_name == d[0]):
             nodes.append(Node(d[0]))
-        if not any(_ for _ in nodes if _.start == d[1]):
+        if not any(_ for _ in nodes if _.node_name == d[1]):
             nodes.append(Node(d[1]))
 
     for d in data:
-        start = [_ for _ in nodes if _.start == d[0]]
-        end = [_ for _ in nodes if _.start == d[1]]
-        start[0].add_next(end)
+        start = next(_ for _ in nodes if _.node_name == d[0])
+        end = [_ for _ in nodes if _.node_name == d[1]]
+        start.add_next(end)
         if len(end) == 1:
-            end[0].add_next(start)
+            end[0].add_next([start])
 
-    # for n in nodes:
-    #     print(n)
-    # print("===")
-    start = [_ for _ in nodes if _.start == "start"][0]
-    return start
+    first_node = next(_ for _ in nodes if _.is_start)
+    return first_node
 
 
 @timeit
-def star1(start):
-    print(traverse(start, "end", [start], check_star1))
+def star1(first_node):
+    print(traverse(first_node, "end", [first_node], check_star1))
 
 
 @timeit
-def star2(start):
-    print(traverse(start, "end", [start], check_star2))
+def star2(first_node):
+    print(traverse(first_node, "end", [first_node], check_star2))
 
 
-start = make_nodes(data)
+first_node = make_nodes(data)
 
-star1(start)
-star2(start)
+star1(first_node)
+star2(first_node)
