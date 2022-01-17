d6e2757b4e9d6b3e3ba1e322143223342bbb92f4
Jesper Högström <jesper.hogstrom@aditrologistics.com>
2022-01-17 14:27:50 +0100

    code cleanup

--- 2021/dec25.py
+++ 2021/dec25.py
@@ -13,42 +13,47 @@ data = [_.strip() for _ in open(filename, 'r').readlines()]
 if not data:
     raise FileNotFoundError(f"No data in {dataname}")
 
-def constant_factory(value):
-    return lambda: value
 
-# class Board():
-#     def __init__(self, data) -> None:
-#         pass
+RIGHT = ">"
+DOWN = "v"
 
-moved = False
+def get_next_c_down(board, c, xmax, ymax):
+    resy = c[1]+1 if c[1] < ymax-1 else 0
+    new_c = (c[0], resy)
+    if not board[new_c]:
+        # print(f"{breed} @ {(x, y)} moves to {(x, resy)}")
+        return new_c, True
+    return c, False
 
-def get_next_c(board, x, y, breed, xmax, ymax):
-    global moved
-    if breed == ">":
-        resx = x+1 if x < xmax-1 else 0
-        if board[(resx, y)] == ".":
-            # print(f"{breed} @ {(x, y)} moves to {(resx, y)}")
-            moved = True
-            return (resx, y)
-        return (x, y)
-    if breed == "v":
-        resy = y+1 if y < ymax-1 else 0
-        if board[(x, resy)] == ".":
-            # print(f"{breed} @ {(x, y)} moves to {(x, resy)}")
-            moved = True
-            return (x, resy)
-        return x, y
-    raise
 
-def move_cucumbers(board, breed, *, xmax, ymax, nextgen):
-    # hasmoved = False
+def get_next_c_right(board, c, xmax, ymax):
+    resx = c[0]+1 if c[0] < xmax-1 else 0
+    new_c = (resx, c[1])
+    if not board[new_c]:
+        # print(f"{breed} @ {(x, y)} moves to {(resx, y)}")
+        return new_c, True
+    return c, False
+
+
+get_next_c = {
+    DOWN: get_next_c_down,
+    RIGHT: get_next_c_right
+}
+
+
+def move_cucumbers(board, breed, *, xmax, ymax):
+    hasmoved = False
+    nextgen = []
+
     for x in range(xmax):
         for y in range(ymax):
-            if board[(x, y)] == breed:
-                next_c = get_next_c(board, x, y, breed, xmax, ymax)
-                nextgen[next_c] = breed
-                # hasmoved |= next_c == (x, y)
-    # return hasmoved
+            c = (x, y)
+            if board[c] == breed:
+                next_c, didMove = get_next_c[breed](board, c, xmax, ymax)
+                hasmoved |= didMove
+                nextgen.append(next_c)
+    return hasmoved, nextgen
+
 
 def print_out(gencount, board, xmax, ymax):
     print(f"After {gencount} steps")
@@ -58,50 +63,35 @@ def print_out(gencount, board, xmax, ymax):
         print()
     print("===")
 
-def update_board(board, nextgen, breed):
-    moved = [_ for _ in board if board[_] == breed]
-    for _ in moved:
-        board.pop(_)
+
+def update_board(board: dict, nextgen: list, breed):
+    res = defaultdict(bool)
+    for _, v in board.items():
+        if v != breed:
+            res[_] = v
     for _ in nextgen:
-        board[_] = nextgen[_]
+        res[_] = breed
+    return res
 
 
 @timeit
 def star1(data):
-    global moved
-    board = defaultdict(constant_factory('.'))
+    board = defaultdict(bool)
     for y in range(len(data)):
         for x in range(len(data[y])):
-            board[(x, y)] = data[y][x]
-    xmax, ymax = x+1, y+1
-    moved = True
-    gencount = 0
+            board[(x, y)] = False if data[y][x] == '.' else data[y][x]
+    xmax, ymax, moved_right, moved_down, gencount = x+1, y+1, True, True, 0
     # print_out(gencount, board, xmax, ymax)
-    while moved:
-        moved = False
+    while moved_right or moved_down:
         gencount += 1
-        nextgen = defaultdict(constant_factory('.'))
-        move_cucumbers(board, ">",
-            xmax=xmax, ymax=ymax, nextgen=nextgen)
-        update_board(board, nextgen, ">")
+        moved_right, nextgen = move_cucumbers(board, RIGHT, xmax=xmax, ymax=ymax)
+        board = update_board(board, nextgen, RIGHT)
 
-        nextgen = defaultdict(constant_factory('.'))
-        move_cucumbers(board, "v",
-            xmax=xmax, ymax=ymax, nextgen=nextgen)
-        update_board(board, nextgen, "v")
+        moved_down, nextgen = move_cucumbers(board, DOWN, xmax=xmax, ymax=ymax)
+        board = update_board(board, nextgen, DOWN)
 
         # print_out(gencount, board, xmax, ymax)
-        if gencount == 10000:
-            exit()
     print(gencount)
 
 
-
-@timeit
-def star2(data):
-    ...
-
-
-data2 = data[:]
 star1(data)
-star2(data2)
