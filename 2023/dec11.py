29d4ecad6c460c86f5f7cd765519ab3dba4e8120
Jesper Hogstrom <557619+jhogstrom@users.noreply.github.com>
2023-12-11 23:56:42 +0100

    dec 11 - generalized

--- 2023/dec11.py
+++ 2023/dec11.py
@@ -1,4 +1,3 @@
-from collections import defaultdict, deque
 import itertools
 import logging
 from reader import get_data, timeit, set_logging
@@ -9,12 +8,15 @@ year = "2023"
 
 set_logging(runtest)
 data = get_data(stardate, year, runtest)
-data2 = data[:]
 
 
-def isempty(data, x):
-    for y in range(len(data)):
-        if data[y][x] == "#":
+def empty_row(data, y):
+    return "#" not in data[y]
+
+
+def empty_col(data, x):
+    for line in data:
+        if line[x] == "#":
             return False
     return True
 
@@ -28,83 +30,46 @@ def print_universe(data):
         print(f"{y:3} {line}")
     print("===")
 
-@timeit
-def star1(data):
-    logging.debug("running star 1")
-    # print_universe(data)
-    # Expand universe
-    expanded = []
-    for line in data:
-        expanded.append(line)
-        if "#" not in line:
-            expanded.append(line)
-    data = expanded[:]
-    expanded = ["" for _ in data]
-    for x in range(len(data[0])):
-        empty = isempty(data, x)
-        for _ in range(len(expanded)):
-            expanded[_] += data[_][x]
-            if empty:
-                expanded[_] += "."
-    data = expanded[:]
-    # print_universe(data)
+
+def calculate_distances(data, hubble):
+    # Calculate expansion matrix
+    matrix = []
+    for y in range(len(data)):
+        line = []
+        for x in range(len(data[y])):
+            if empty_col(data, x) or empty_row(data, y):
+                line.append(hubble)
+            else:
+                line.append(1)
+        matrix.append(line)
     # Find galaxies
     galaxies = []
     for y in range(len(data)):
         for x in range(len(data[y])):
             if data[y][x] == "#":
                 galaxies.append((x, y))
-    # print(galaxies)
-    # Find pairs
+    # print_universe(data)
     res = 0
+    # Calculate all distances
     for p in itertools.combinations(galaxies, 2):
-        d = abs(p[0][0] - p[1][0]) + abs(p[0][1] - p[1][1])
-        res += d
+        x = matrix[p[0][1]][min([p[0][0], p[1][0]])+1:max([p[0][0], p[1][0]])+1]
+        y = [matrix[_][p[0][0]] for _ in range(min([p[0][1], p[1][1]])+1, max([p[0][1], p[1][1]])+1)]
+        res += sum(x) + sum(y)
 
-    print(res)
+    # print_universe(matrix)
+    return res
+
+@timeit
+def star1(data):
+    logging.debug("running star 1")
+    print(calculate_distances(data, 2))
 
 
 @timeit
 def star2(data):
     logging.debug("running star 2")
-    # print_universe(data)
-    # Expand universe
-    expanded = []
-    for line in data:
-        expanded.append(line)
-        if "#" not in line:
-            expanded.append("x"*len(line))
-    data = expanded[:]
-    expanded = ["" for _ in data]
-    for x in range(len(data[0])):
-        empty = isempty(data, x)
-        for _ in range(len(expanded)):
-            expanded[_] += data[_][x]
-            if empty:
-                expanded[_] += "x"
-    data = expanded[:]
-    # print_universe(data)
-    # Find galaxies
-    galaxies = []
-    for y in range(len(data)):
-        for x in range(len(data[y])):
-            if data[y][x] == "#":
-                galaxies.append((x, y))
-    # print(galaxies)
-    # Find pairs
-    res = 0
-    hubble = 1000_000
-    for p in itertools.combinations(galaxies, 2):
-        x_string = data[p[0][1]][min([p[0][0], p[1][0]])+1:max([p[0][0],p[1][0]])+1]
-        y_string = "".join([data[_][p[0][0]] for _ in range(min([p[0][1], p[1][1]])+1, max([p[0][1], p[1][1]])+1)])
-        xdist = sum([1 if _ in "#." else hubble-1 for _ in x_string])
-        ydist = sum([1 if _ in "#." else hubble-1 for _ in y_string])
-        d = xdist + ydist
-        res += d
-
-    print(res)
-
+    print(calculate_distances(data, 1_000_000))
 
 
 star1(data)
-star2(data2)
+star2(data)
