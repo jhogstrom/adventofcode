98cc8bbb0875f455ae1cceb5c14e3dd7f7bdffb1
Jesper Hogstrom <557619+jhogstrom@users.noreply.github.com>
2023-12-14 23:25:33 +0100

    dec 14 - optimized and cleaned up

--- 2023/dec14.py
+++ 2023/dec14.py
@@ -1,4 +1,3 @@
-from collections import defaultdict, deque
 import logging
 from reader import get_data, timeit, set_logging
 
@@ -8,28 +7,22 @@ year = "2023"
 
 set_logging(runtest)
 data = get_data(stardate, year, runtest)
-data2 = data[:]
 
-
-def next_cell(p, direction):
-    if direction == "south":
-        return (p[0], p[1]+1)
-    if direction == "west":
-        return (p[0]-1, p[1])
-    if direction == "east":
-        return (p[0]+1, p[1])
-    if direction == "north":
-        return (p[0], p[1]-1)
-    raise ValueError(f"Unknown direction {direction}")
+moves = {
+    "south": lambda p: (p[0], p[1]+1),
+    "west": lambda p: (p[0]-1, p[1]),
+    "east": lambda p: (p[0]+1, p[1]),
+    "north": lambda p: (p[0], p[1]-1),
+}
 
 
 def move_cell(p, matrix, direction):
     # logging.debug(f"Moving cell {x}, {y}")
     if matrix[p] != "O":
         return
-    while matrix.get(next_cell(p, direction), "#") == ".":
+    while matrix.get(moves[direction](p), "#") == ".":
         matrix[p] = "."
-        p = next_cell(p, direction)
+        p = moves[direction](p)
         matrix[p] = "O"
 
 
@@ -85,65 +78,52 @@ def tilt(direction, matrix, data):
 def star1(data):
     logging.debug("running star 1")
     matrix = define_matrix(data)
-    matrix = tilt("north", matrix, data)
+    tilt("north", matrix, data)
     print(board_load(matrix, data))
 
 
 def cycle_board(matrix, data):
-    matrix = tilt("north", matrix, data)
-    matrix = tilt("west", matrix, data)
-    matrix = tilt("south", matrix, data)
-    matrix = tilt("east", matrix, data)
-    return matrix
+    tilt("north", matrix, data)
+    tilt("west", matrix, data)
+    tilt("south", matrix, data)
+    tilt("east", matrix, data)
 
 
 def detect_loop(thelist):
-    looplen = 2
-    fast = 1
-    slow = 0
-    mu = 0
+    slow, fast = 0, 1
     loop1 = False
-    lam = 0
-    while fast+looplen < len(thelist) and slow < len(thelist):
-        fast += looplen
+    period = 0
+    maxfast = len(thelist) - 2
+    while fast < maxfast:
+        fast += 2
         slow += 1
-        mu += 1
-        lam += 1
-        if thelist[fast] == thelist[slow] and lam > 1:
-            if loop1:
-                # print(f"Start pos: {mu-lam} - len = {lam}")
-                return (True, mu-lam, lam)
+        period += 1
+        if thelist[fast] == thelist[slow] and period > 1:
+            if not loop1:
+                period = 0
             else:
-                lam = 0
+                return (True, slow-period, period)
             loop1 = True
-    return (False, 0, 0)
-
+    return (False, -1, -1)
 
 @timeit
 def star2(data):
     logging.debug("running star 2")
     matrix = define_matrix(data)
-    c = 0
     loads = []
 
     while not detect_loop(loads)[0]:
         cycle_board(matrix, data)
-        c += 1
         loads.append(board_load(matrix, data))
-        # print_board(matrix, data)
-        print(c, loads[-1])
 
     MAXITER = 1_000_000_000
     _, loop_start, period = detect_loop(loads)
-    print(f"Loop start: {loop_start}. Period:  {period}")
-    index1 = (MAXITER - loop_start) % period
-    index2 = MAXITER - (MAXITER//period)*period
+    # print(f"Loop start: {loop_start}. Period:  {period}")
+    index = MAXITER - (MAXITER//period)*period
 
-    print(index1, index2)
-    print("index1: ", loads[loop_start + index1])
-    print("index2 ", loads[loop_start + index2])
-    print("104639 is too high")
+    print(loads[loop_start + index])
+    print(len(loads))
 
 
 star1(data)
-star2(data2)
+star2(data)
