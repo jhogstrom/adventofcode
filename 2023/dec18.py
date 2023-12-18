906746cc462d00a4be2de83574d5bff54285d448
Jesper Hogstrom <557619+jhogstrom@users.noreply.github.com>
2023-12-18 17:44:45 +0100

    dec 18 - refactored to one solution

--- 2023/dec18.py
+++ 2023/dec18.py
@@ -1,4 +1,3 @@
-from collections import defaultdict, deque
 import logging
 from reader import get_data, timeit, set_logging
 
@@ -8,67 +7,34 @@ year = "2023"
 
 set_logging(runtest)
 data = get_data(stardate, year, runtest)
-data2 = data[:]
-
-moves = {
-    "R": lambda p: (p[0]+1, p[1]),
-    "L": lambda p: (p[0]-1, p[1]),
-    "U": lambda p: (p[0], p[1]-1),
-    "D": lambda p: (p[0], p[1]+1),
-}
 
 
-@timeit
-def star1(data):
-    logging.debug("running star 1")
-    p = (0, 0)
-    frame = {p}
-
+def star1_instructions(data):
+    res = []
     for instr in data:
         d, length, _ = instr.split()
         length = int(length)
-        for _ in range(length):
-            p = moves[d](p)
-            frame.add(p)
-
-    filled = 0
-    for y in range(min([_[1] for _ in frame]), max([_[1] for _ in frame])+1):
-        inside = False
-        turns = []
-        for x in range(min([_[0] for _ in frame]), max([_[0] for _ in frame])+1):
-            if (x, y) in frame:
-                filled += 1
-                if (x, y-1) in frame:
-                    turns.append("UP")
-                if (x, y+1) in frame:
-                    turns.append("DOWN")
-                if len(turns) == 2 and (x+1, y) not in frame:
-                    if "UP" in turns and "DOWN" in turns:
-                        inside = not inside
-                    turns = []
-                    # print("X", end="")
-                # else:
-                #     print("#", end="")
-            else:
-                if inside:
-                    filled += 1
-                #     print(".", end="")
-                # else:
-                #     print(" ", end="")
-        # print()
-    print(filled)
+        res.append((d, length))
+    return res
 
 
-@timeit
-def star2(data):
-    logging.debug("running star 2")
-    p = (0, 0)
-    corners = [p]
-    framelen = 0
+def star2_instructions(data):
+    res = []
     for instr in data:
         _, _, i = instr.split()
         length = int(i[2:-2], 16)
         d = "RDLU"[int(i[-2])]
+        res.append((d, length))
+    return res
+
+
+def docalc(data, instruction_extractor):
+    instructions = instruction_extractor(data)
+    p = (0, 0)
+    corners = [p]
+    framelen = 0
+    for instr in instructions:
+        d, length = instr
         if d == "R":
             p = (p[0]+length, p[1])
         elif d == "L":
@@ -84,5 +50,17 @@ def star2(data):
     print(area)
 
 
+@timeit
+def star1(data):
+    logging.debug("running star 1")
+    docalc(data, star1_instructions)
+
+
+@timeit
+def star2(data):
+    logging.debug("running star 2")
+    docalc(data, star2_instructions)
+
+
 star1(data)
-star2(data2)
+star2(data)
