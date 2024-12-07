28bda5ffd51a1a4421fa86e60dc8ba22728384f4
Jesper Hogstrom <557619+jhogstrom@users.noreply.github.com>
2024-12-08 00:41:16 +0100

    Dec 7-2024 - Better optimizations

--- 2024/dec07.py
+++ 2024/dec07.py
@@ -11,24 +11,45 @@ data = get_data(stardate, year, runtest)
 data2 = data[:]
 
 
-def evaluate(expected_value: int, lvalue: int, terms: list[int], ops: list[str]) -> int:
-    if not terms:
-        return lvalue if lvalue == expected_value else 0
-    for o in ops:
-        if o == "+":
-            next_lvalue = lvalue + terms[0]
-        elif o == "*":
-            next_lvalue = lvalue * terms[0]
-        elif o == "||":
-            next_lvalue = int(str(lvalue) + str(terms[0]))
-        else:
-            raise ValueError(f"Unknown operator '{o}'")
-        res = evaluate(expected_value, next_lvalue, terms[1:], ops)
-        if res == expected_value:
-            return res
-        if res > expected_value:
-            return 0
-    return 0
+# def evaluate(expected_value: int, lvalue: int, terms: list[int], ops: list[str]) -> int:
+#     if not terms:
+#         return lvalue if lvalue == expected_value else 0
+#     if lvalue > expected_value:
+#         return 0
+#     for o in ops:
+#         if o == "+":
+#             next_lvalue = lvalue + terms[0]
+#         elif o == "*":
+#             next_lvalue = lvalue * terms[0]
+#         elif o == "||":
+#             next_lvalue = int(str(lvalue) + str(terms[0]))
+#         else:
+#             raise ValueError(f"Unknown operator '{o}'")
+#         res = evaluate(expected_value, next_lvalue, terms[1:], ops)
+#         if res == expected_value:
+#             return res
+#     return 0
+
+
+def evaluate(
+    expected_value: int, lvalue: int, terms: list[int], allow_concat: bool = False
+) -> bool:
+    if lvalue > expected_value:
+        return False
+    next_terms = terms[1:]
+    if not next_terms:
+        return lvalue == expected_value
+    t0 = terms[0]
+    return (
+        evaluate(expected_value, lvalue + t0, next_terms, allow_concat)
+        or evaluate(expected_value, lvalue * t0, next_terms, allow_concat)
+        or (
+            allow_concat
+            and evaluate(
+                expected_value, int(str(lvalue) + str(t0)), next_terms, allow_concat
+            )
+        )
+    )
 
 
 def parse(s: str) -> tuple[int, list[int]]:
@@ -44,20 +65,22 @@ def star1(data):
     result = 0
     for s in data:
         expected_value, terms = parse(s)
-        result += evaluate(expected_value, terms[0], terms[1:], ["+", "*"])
+        if evaluate(expected_value, terms[0], terms[1:]):
+            result += expected_value
     print(result)
 
 
 @timeit
 def star2(data):
+    pass
     logging.debug("running star 2")
     result = 0
     for s in data:
         expected_value, terms = parse(s)
-        if evaluate(expected_value, terms[0], terms[1:], ["+", "*"]):
+        if evaluate(expected_value, terms[0], terms[1:]):
             result += expected_value
-        else:
-            result += evaluate(expected_value, terms[0], terms[1:], ["+", "*", "||"])
+        elif evaluate(expected_value, terms[0], terms[1:], True):
+            result += expected_value
     print(result)
 
 
